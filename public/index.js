405ae9b1a61153159b1ca61e37404f6e8e7d2e8e
giladaube <39123326+giladaube@users.noreply.github.com>
2020-09-29 22:14:36 +0300

    first commit

---
+++ public/index.js
@@ -0,0 +1,38 @@
+
+
+var elements = ["summary", "experience", "military", "education", "knowledge", "language"];
+var height;
+if (window.innerWidth > 350) {
+  height = -350;
+} else {
+  height = -1200;
+}
+
+var windowHeight;
+$(document).scroll(function() {
+  windowHeight = window.innerHeight;
+  checkPosition();
+})
+
+
+function checkPosition() {
+  for (var i = 0; i < elements.length; i++) {
+    var element = document.querySelector("#" + elements[i] + " .hidden");
+    if (element != null) {
+      var positionFromTop = element.getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= height) {
+        element.classList.add("display-none");
+        element.classList.remove("hidden");
+        $("#" + elements[i] + " .display-none").show(1500);
+      }
+    }
+    if (i % 2 != 0) {
+      $("#" + elements[i]).addClass("color-section2");
+      $("#" + elements[i] + " h1").addClass("font-Big-Heading-color2");
+      $("#" + elements[i] + " p").addClass("font-text-color2");
+    } else {
+      $("#" + elements[i] + " p").addClass("font-text-color1");
+      $("#" + elements[i] + " h1").addClass("font-Big-Heading-color1");
+    }
+  }
+}
