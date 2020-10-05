d4dce1c3c25f1a255bc69c399be2e1bdcf321d31
giladaube <gilad179@gmail.com>
2020-10-05 15:30:38 +0300

    add function and support for mobile view

--- public/index.js
+++ public/index.js
@@ -1,45 +1,106 @@
 
-
 var elements = ["summary", "experience", "military", "education", "knowledge", "language"];
 var height;
+var windowHeight;
 var notMobileView = true;
+// checks if the current view is in a mobile device
 var media = window.matchMedia("only screen and (max-device-width: 500px)");
+// if it is a mobile device, it won't use the animation.
 if (media.matches) {
   notMobileView = false;
 } else {
-  height = -400;
+  height = -390;
+}
+// what to do while using mobile view
+if (!notMobileView) {
+  goThroughElements(routineMobileView);
 }
 
-var windowHeight;
 $(document).scroll(function() {
-  windowHeight = window.innerHeight;
-  checkPosition();
+  if (notMobileView) {
+    goThroughElements(routineNotMobileView);
+  }
 })
 
+// go through all elements and run the given function
+function goThroughElements(runFunction) {
+  for (let i = 0; i < elements.length; i++) {
+    runFunction(i);
+    // add style to the content
+    addStyle(i);
+  }
+}
 
+// what to do if the view is in mobile
+function routineMobileView(index) {
+  let element = getDocElement(index, " .hidden");
+  // make sure the given element is hidden
+  if (element != null) {
+    element.classList.remove("hidden");
+  }
+}
 
-function checkPosition() {
-  for (var i = 0; i < elements.length; i++) {
-    var element = document.querySelector("#" + elements[i] + " .hidden");
-    if (element != null && notMobileView) {
-      var positionFromTop = element.getBoundingClientRect().top;
-      if (positionFromTop - windowHeight <= height) {
-        element.classList.add("display-none");
-        element.classList.remove("hidden");
-        if (i % 2 != 0) {
-          $("#" + elements[i] + " .display-none").show(1200);
-        } else {
-          $("#" + elements[i] + " .display-none").slideDown(1200);
-        }
-      }
+// what to do if the view isn't in mobile
+function routineNotMobileView(index) {
+  let element = getDocElement(index, " .hidden");
+  // make sure the given element is hidden
+  if (element != null) {
+    windowHeight = window.innerHeight;
+    if (getBoundingTop(index) - windowHeight <= height) {
+      element.classList.add("display-none");
+      element.classList.remove("hidden");
+      // add animation to show the content
+      addAnimation(index);
     }
-    if (i % 2 != 0) {
-      $("#" + elements[i]).addClass("color-section2");
-      $("#" + elements[i] + " h1").addClass("font-Big-Heading-color2");
-      $("#" + elements[i] + " p").addClass("font-text-color2");
-    } else {
-      $("#" + elements[i] + " p").addClass("font-text-color1");
-      $("#" + elements[i] + " h1").addClass("font-Big-Heading-color1");
-    }
+  }
+}
+
+// get an element based on the index in "elements"
+function getElements(index) {
+  return $("#" + elements[index]);
+}
+// get an element based on the index in "elements" and a className in Query format
+function getElement(index, classNameQuery) {
+  return $("#" + elements[index] + classNameQuery);
+}
+
+// get an element based on the index in "elements" and a className in Query format (not with jQuery)
+function getDocElement(index, classNameQuery) {
+  return document.querySelector("#" + elements[index] + classNameQuery);
+}
+
+// get the positionFromTop for a given element (based on a given index for elements)
+function getBoundingTop(index) {
+  return getElements(index).get(0).getBoundingClientRect().top;
+}
+
+// add className to an element (getting it based on index and classNameQuery using getElement func)
+function addClassElement(index, classNameQuery, addClassName) {
+  getElement(index, classNameQuery).addClass(addClassName);
+}
+
+// remove className from an element (getting it based on index and classNameQuery using getElement func)
+function removeClassElement(index, classNameQuery, removeClassName) {
+  getElement(index, classNameQuery).removeClass(removeClassName);
+}
+
+// add animation to an element based on the index
+function addAnimation(index) {
+  if (index % 2 != 0) {
+    getElement(index, " .display-none").show(1200);
+  } else {
+    getElement(index, " .display-none").slideDown(1200);
+  }
+}
+
+// add a different style based on the index
+function addStyle(index) {
+  if (index % 2 != 0) {
+    addClassElement(index, "", "color-section2");
+    addClassElement(index, " h1", "font-Big-Heading-color2");
+    addClassElement(index, " p", "font-text-color2");
+  } else {
+    addClassElement(index, " h1", "font-Big-Heading-color1");
+    addClassElement(index, " p", "font-text-color1");
   }
 }
